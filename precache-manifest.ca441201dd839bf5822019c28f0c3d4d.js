aca2ba7e554ecd415ad64211b666fdf81cf0d6b4
k-jasinska <jasinskak64@gmail.com>
2019-04-07 21:45:09 +0200

    Updates

---
+++ precache-manifest.ca441201dd839bf5822019c28f0c3d4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e65861500974d2c47c58",
+    "url": "/k-jasinska/static/css/main.b35828bf.chunk.css"
+  },
+  {
+    "revision": "e65861500974d2c47c58",
+    "url": "/k-jasinska/static/js/main.64c5cb9f.chunk.js"
+  },
+  {
+    "revision": "e6f1a1619ee560a4ae03",
+    "url": "/k-jasinska/static/js/runtime~main.dff59cc0.js"
+  },
+  {
+    "revision": "00445f4ecb4670e1311d",
+    "url": "/k-jasinska/static/css/2.29c9964d.chunk.css"
+  },
+  {
+    "revision": "00445f4ecb4670e1311d",
+    "url": "/k-jasinska/static/js/2.e23f2e18.chunk.js"
+  },
+  {
+    "revision": "c6858db337fa47bf5172a354086aab30",
+    "url": "/k-jasinska/index.html"
+  }
+];
